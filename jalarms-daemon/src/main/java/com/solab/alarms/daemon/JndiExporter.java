9e878647f7321d94fe7f9d5e09f56b7d06ee38f4
Enrique Zamudio <alterbass@gmail.com>
2011-06-30 13:58:31 -0500

    The JNDI export thing didn't work for the jalarms-daemon project, but RMI worked fine.

--- jalarms-daemon/src/main/java/com/solab/alarms/daemon/JndiExporter.java
+++ jalarms-daemon/src/main/java/com/solab/alarms/daemon/JndiExporter.java
@@ -2,32 +2,39 @@ package com.solab.alarms.daemon;
 
 import com.solab.alarms.AlarmSender;
 import javax.annotation.*;
-import org.springframework.jndi.JndiTemplate;
+import javax.naming.*;
+import org.slf4j.*;
 
 /** This component is used to bind the AlarmSender to a JNDI path.
  *
  * @author Enrique Zamudio
  */
 public class JndiExporter {
-	private String path;
-	private final JndiTemplate jndi = new JndiTemplate();
+
+	private final String name;
+	private final Logger log = LoggerFactory.getLogger(getClass());
 	@Resource private AlarmSender bean;
 
-	public JndiExporter(String jndiPath) {
-		path = jndiPath;
+	public JndiExporter(String name) {
+		this.name = name;
 	}
 
 	@PostConstruct
 	public void bindBean() throws javax.naming.NamingException {
-		if (path != null && path.length() > 0) {
-			jndi.bind(path, bean);
+		if (name != null && name.length() > 0) {
+			Context jndi = new InitialContext();
+			jndi = (Context)jndi.lookup("java:");
+			jndi.bind(name, bean);
+			log.info("Bound AlarmSender to JNDI name java:{}", name);
 		}
 	}
 
 	@PreDestroy
 	public void unbindBean() throws javax.naming.NamingException {
-		if (path != null && path.length() > 0) {
-			jndi.unbind(path);
+		if (name != null && name.length() > 0) {
+			InitialContext jndi = new InitialContext();
+			jndi.unbind(name);
+			log.info("Unbound AlarmSender from JNDI {}", name);
 		}
 	}
 
