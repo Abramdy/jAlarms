4b1ca971cb03972d1697de6c2c1ce3f347681bcc
Enrique Zamudio <alterbass@gmail.com>
2011-06-30 20:06:42 -0500

    Added a very basic password auth mechanism to the AlarmServlet.

--- jalarms-service/src/main/java/com/solab/alarms/servlet/AlarmServlet.java
+++ jalarms-service/src/main/java/com/solab/alarms/servlet/AlarmServlet.java
@@ -15,10 +15,13 @@ import org.springframework.beans.factory.config.*;
 public class AlarmServlet extends HttpServlet {
 
 	private AlarmSenderImpl sender;
+	private java.util.Properties authbase;
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		sender = WebApplicationContextUtils.getWebApplicationContext(config.getServletContext()).getBean(AlarmSenderImpl.class);
+		WebApplicationContext ctxt = WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
+		sender = ctxt.getBean(AlarmSenderImpl.class);
+		authbase = (java.util.Properties)ctxt.getBean("servletPasswords");
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,13 +31,20 @@ public class AlarmServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String msg = request.getParameter("alarm");
 		String src = request.getParameter("src");
+		String pass = request.getParameter("password");
 		String resp = "ERROR Incomplete Data, you must send field 'alarm' and optionally 'src'";
 		if (msg != null) {
-			//TODO setup some kind of password for default messages and per-src passwords
-			if (src != null) {
+			String vpass = authbase.getProperty(src == null ? "defaultPassword" : src);
+			boolean ok = true;
+			if (vpass != null && vpass.length() > 0) {
+				ok = vpass.equals(pass);
 			}
-			sender.sendAlarm(msg, src);
-			resp = "OK";
+			if (ok) {
+				sender.sendAlarm(msg, src);
+				resp = "OK";
+			} else {
+				resp = "AUTH failed";
+			}
 		}
 		response.setContentType("text/plain");
 		response.setContentLength(resp.length());
