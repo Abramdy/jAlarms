97ffd9dfe88bbd7775c4bdb62b2faf3be28e19ca
Enrique Zamudio <alterbass@gmail.com>
2011-10-10 17:36:58 -0500

    Register shutdown hook

--- jalarms-protobuf/src/main/java/com/solab/alarms/spring/ProtoBootstrap.java
+++ jalarms-protobuf/src/main/java/com/solab/alarms/spring/ProtoBootstrap.java
@@ -7,6 +7,7 @@ public class ProtoBootstrap {
 	public static void main(String[] args) {
 		String cname = args.length > 0 ? args[0] : "jalarms-protobuf.xml";
 		ClassPathXmlApplicationContext ctxt = new ClassPathXmlApplicationContext(cname);
+		ctxt.registerShutdownHook();
 		((Runnable)ctxt.getBean("main")).run();
 	}
 
