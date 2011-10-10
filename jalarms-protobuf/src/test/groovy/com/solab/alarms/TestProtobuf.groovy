cbac2b00d5a37a8f7b37ae04c08f5124043c4441
Enrique Zamudio <alterbass@gmail.com>
2011-10-10 16:40:06 -0500

    Added test classes, in Groovy.

---
+++ jalarms-protobuf/src/test/groovy/com/solab/alarms/TestProtobuf.groovy
@@ -0,0 +1,20 @@
+package com.solab.alarms
+
+import org.junit.*
+import com.solab.alarms.protobuf.AlarmProtos.Alarm
+
+class TestProtobuf {
+
+	@Test
+	void testProtobuf() {
+		def bout = new ByteArrayOutputStream()
+		Alarm.newBuilder().setAlarm('Test 1').setAlways(false).build().writeDelimitedTo(bout)
+		def bin = new ByteArrayInputStream(bout.toByteArray())
+		Alarm parsed = Alarm.parseDelimitedFrom(bin)
+		assert parsed != null
+		assert parsed.alarm=='Test 1'
+		assert !parsed.hasSource()
+		assert parsed.hasAlways() && !parsed.always
+	}
+
+}
