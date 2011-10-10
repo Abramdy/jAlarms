e3c9edd3d28cfd32eafac2adc534d88c669f2649
Enrique Zamudio <alterbass@gmail.com>
2011-10-10 16:38:07 -0500

    ProtoBootstrap is the main class for the server app, requires Spring

---
+++ jalarms-protobuf/src/main/java/com/solab/alarms/spring/ProtoBootstrap.java
@@ -0,0 +1,13 @@
+package com.solab.alarms.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ProtoBootstrap {
+
+	public static void main(String[] args) {
+		String cname = args.length > 0 ? args[0] : "jalarms-protobuf.xml";
+		ClassPathXmlApplicationContext ctxt = new ClassPathXmlApplicationContext(cname);
+		((Runnable)ctxt.getBean("main")).run();
+	}
+
+}
