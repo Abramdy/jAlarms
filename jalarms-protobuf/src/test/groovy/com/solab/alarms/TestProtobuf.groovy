f25b6436bbfe99e6459ae161fcd2b43aa24fd9e9
Enrique Zamudio <alterbass@gmail.com>
2011-10-10 20:05:06 -0500

    Test prints message length

--- jalarms-protobuf/src/test/groovy/com/solab/alarms/TestProtobuf.groovy
+++ jalarms-protobuf/src/test/groovy/com/solab/alarms/TestProtobuf.groovy
@@ -10,6 +10,7 @@ class TestProtobuf {
 		def bout = new ByteArrayOutputStream()
 		Alarm.newBuilder().setAlarm('Test 1').setAlways(false).build().writeDelimitedTo(bout)
 		def bin = new ByteArrayInputStream(bout.toByteArray())
+		println "El mensaje mide ${bout.toByteArray().length} bytes"
 		Alarm parsed = Alarm.parseDelimitedFrom(bin)
 		assert parsed != null
 		assert parsed.alarm=='Test 1'
