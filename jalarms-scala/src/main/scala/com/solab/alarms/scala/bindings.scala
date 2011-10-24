8d04fadf31569d97b65359042d28381a3aed8adb
Enrique Zamudio <alterbass@gmail.com>
2011-10-24 11:28:35 -0500

    The bindings file includes an object with an implicit conversion so that AlarmSender appears to have a new method "withAlarm"

---
+++ jalarms-scala/src/main/scala/com/solab/alarms/scala/bindings.scala
@@ -0,0 +1,27 @@
+package com.solab.alarms.scala
+
+import com.solab.alarms.AlarmSender
+
+object bindings {
+
+	implicit def bindAlarmSender(sender:AlarmSender) = new ScalarmSender(sender)
+
+}
+
+class ScalarmSender(sender:AlarmSender) {
+
+	def withAlarm(alarm:String, source:String)(body: => Unit) {
+		try {
+			body
+		} catch {
+			case e =>
+				sender.sendAlarm(alarm, source)
+				throw e
+		}
+	}
+
+	def withAlarm(alarm:String)(f: => Unit) {
+		withAlarm(alarm, null)(f)
+	}
+
+}
