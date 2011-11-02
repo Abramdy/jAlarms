cb1495259ba435af6b9fe831514b1291344dd5f9
Enrique Zamudio <alterbass@gmail.com>
2011-11-02 15:14:27 -0600

    Added configPath and cacheName properties, with default values.

--- jalarms-channels/src/main/java/com/solab/alarms/AlarmEhcacheClient.java
+++ jalarms-channels/src/main/java/com/solab/alarms/AlarmEhcacheClient.java
@@ -1,5 +1,11 @@
 package com.solab.alarms;
 
+import javax.annotation.*;
+
+import org.slf4j.LoggerFactory;
+
+import net.sf.ehcache.*;
+
 /** An alarm cache that uses ehcache to store the data it needs to know if an alarm message should be
  * resent. This is useful in environments where you have several applications which can be using similar
  * alarm channels, since the DefaultAlarmCache is internal to the app and events in two or more apps
@@ -9,6 +15,31 @@ package com.solab.alarms;
  *         Date: 02/11/11 14:33
  */
 public class AlarmEhcacheClient implements AlarmCache {
+
+	private final org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
+	private CacheManager cacheman;
+	private Ehcache cache;
+	private String configPath = "/jalarms_ehcache.xml";
+	private String cacheName = "jalarms";
+
+	/** Sets the path of the config file, located in the classpath. Default is "/jalarms_ehcache.xml". */
+	public void setConfigPath(String path) {
+		configPath = path;
+	}
+	public String getConfigPath() { return configPath; }
+
+	/** Sets the name of the cache to use. Default is "jalarms". */
+	public void setCacheName(String name) {
+		cacheName = name;
+	}
+	public String getCacheName() { return cacheName; }
+
+	@PostConstruct
+	public void init() {
+		cacheman = CacheManager.create(getClass().getResource(configPath));
+		cache = cacheman.getEhcache("jalarms");
+	}
+
     @Override
     public void store(AlarmChannel channel, String source, String message) {
         //To change body of implemented methods use File | Settings | File Templates.
