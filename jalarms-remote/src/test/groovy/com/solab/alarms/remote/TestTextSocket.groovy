20aef8b11d475d2c68cb3a2fa57f41076ed227e9
Enrique Zamudio <alterbass@gmail.com>
2011-11-22 21:03:53 -0600

    Unit tests for the text socket, done with Spock

---
+++ jalarms-remote/src/test/groovy/com/solab/alarms/remote/TestTextSocket.groovy
@@ -0,0 +1,44 @@
+package com.solab.alarms.remote
+
+import com.solab.alarms.AlarmSenderImpl
+import com.solab.alarms.channels.TestChannel
+import java.io.*
+import java.net.Socket
+import spock.lang.*
+
+/** Unit tests for the TextSocketListener.
+ *
+ * @author Enrique Zamudio */
+class TestTextSocket extends Specification {
+
+	@Shared AlarmSenderImpl sender = new AlarmSenderImpl()
+	@Shared TextSocketListener server = new TextSocketListener(65000)
+
+	void setupSpec() {
+		sender.alarmChannels = [ new TestChannel() ]
+		server.alarmSender = sender
+		server.startListening()
+	}
+
+	def "send alarms"() {
+		given:
+			Socket sock = new Socket("127.0.0.1", 65000)
+			def pout = new PrintStream(sock.outputStream)
+			def sin = new BufferedReader(new InputStreamReader(sock.inputStream))
+		when:
+			pout.println(alarm)
+			pout.flush()
+		then:
+			sin.readLine() == "OK"
+		cleanup:
+			sock.close()
+		where:
+			alarm << ["alarm without source", "source::alarm with source"]
+	}
+
+	void cleanupSpec() {
+		server.shutdown()
+		sender.shutdown()
+	}
+
+}
