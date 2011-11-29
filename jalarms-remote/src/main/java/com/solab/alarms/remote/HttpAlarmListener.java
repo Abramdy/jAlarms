46eb94593c5b2c1edffabaf5e12db1fc9fd77069
Enrique Zamudio <alterbass@gmail.com>
2011-11-29 15:19:29 -0600

    Implement the HTTP server using JBoss Netty

--- jalarms-remote/src/main/java/com/solab/alarms/remote/HttpAlarmListener.java
+++ jalarms-remote/src/main/java/com/solab/alarms/remote/HttpAlarmListener.java
@@ -1,20 +1,37 @@
 package com.solab.alarms.remote;
 
+import com.solab.alarms.remote.http.HttpRequestHandler;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+
 import javax.annotation.PreDestroy;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import static org.jboss.netty.channel.Channels.pipeline;
 
 /** A very simple remote alarm listener that dispatches HTTP requests.
  *
  * @author Enrique Zamudio
  */
-public class HttpAlarmListener extends AbstractAlarmListener {
+public class HttpAlarmListener extends AbstractAlarmListener implements ChannelPipelineFactory {
 
 	private final int port;
+    private final ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
+            Executors.newCachedThreadPool(), tpool));
+    
 
 	public HttpAlarmListener(int tcpPort) {
 		port = tcpPort;
 	}
 
 	public void run() {
+        bootstrap.setPipelineFactory(this);
+        bootstrap.bind(new InetSocketAddress(port));
 	}
 
 	@PreDestroy
@@ -22,5 +39,13 @@ public class HttpAlarmListener extends AbstractAlarmListener {
 		tpool.shutdown();
 	}
 
+    @Override
+    public ChannelPipeline getPipeline() throws Exception {
+        ChannelPipeline pl = pipeline();
+        pl.addLast("decoder", new HttpRequestDecoder());
+        pl.addLast("encoder", new HttpResponseEncoder());
+        pl.addLast("handler", new HttpRequestHandler(getAlarmSender()));
+        return pl;
+    }
 }
 
---
+++ jalarms-remote/src/main/java/com/solab/alarms/remote/http/HttpRequestHandler.java
@@ -0,0 +1,51 @@
+package com.solab.alarms.remote.http;
+
+import com.solab.alarms.AlarmSender;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+import org.jboss.netty.handler.codec.http.*;
+import org.jboss.netty.util.CharsetUtil;
+
+import java.util.List;
+import java.util.Map;
+
+/** Manejador de peticiones HTTP.
+ *
+ * @author Enrique Zamudio
+ *         Date: 29/11/11 14:55
+ */
+public class HttpRequestHandler extends SimpleChannelUpstreamHandler {
+
+    private static final String okResp = "OK";
+    private static final String emptyResp = "You must send 'alarm' and optionally 'src' in the query string of the URL";
+    private final AlarmSender sender;
+
+    public HttpRequestHandler(AlarmSender sender) {
+        this.sender = sender;
+    }
+
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent ev) {
+        final HttpRequest req = (HttpRequest)ev.getMessage();
+        final QueryStringDecoder decoder = new QueryStringDecoder(req.getUri());
+        final Map<String, List<String>> params = decoder.getParameters();
+        String alarm = params.get("alarm") != null && params.get("alarm").size() > 0 ? params.get("alarm").get(0) : null;
+        String src = params.get("src") != null && params.get("src").size() > 0 ? params.get("src").get(0) : null;
+        String resp = okResp;
+        if (alarm == null) {
+            resp = emptyResp;
+        } else if (src == null) {
+            sender.sendAlarm(alarm);
+        } else {
+            sender.sendAlarm(alarm, src);
+        }
+        HttpResponse htresp = new DefaultHttpResponse(req.getProtocolVersion(), HttpResponseStatus.OK);
+        htresp.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain");
+        htresp.setContent(ChannelBuffers.copiedBuffer(resp.toCharArray(), CharsetUtil.UTF_8));
+        htresp.setHeader(HttpHeaders.Names.CONTENT_LENGTH, htresp.getContent().readableBytes());
+        ev.getChannel().write(htresp).addListener(ChannelFutureListener.CLOSE);
+    }
+
+}
