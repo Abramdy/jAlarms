e2e90bd5db1c1a1fc793ec0bab0a8cac90168e7d
Enrique Zamudio <alterbass@gmail.com>
2012-12-19 13:05:32 -0600

    Update versions of dependencies, require Java 7

--- jalarms-protobuf/run-server.sh
+++ jalarms-protobuf/run-server.sh
@@ -1,5 +1,5 @@
 SPRING=`ls -1 lib/spring/*.jar|xargs|sed "s/ /:/g"`
 PROTOBUF=lib/protobuf.jar
-SLF4J=lib/slf4j-1.6.1/slf4j-api-1.6.1.jar:lib/slf4j-1.6.1/slf4j-simple-1.6.1.jar:lib/slf4j-1.6.1/jcl-over-slf4j-1.6.1.jar
+SLF4J=lib/slf4j-1.7.1/slf4j-api-1.7.1.jar:lib/slf4j-1.7.1/slf4j-simple-1.7.1.jar:lib/slf4j-1.7.1/jcl-over-slf4j-1.7.1.jar
 JALARMS=../jalarms-core/build/classes/main
 java -cp $SPRING:$PROTOBUF:$SLF4J:$JALARMS:build/classes/main:src/main/resources com.solab.alarms.spring.ProtoBootstrap
