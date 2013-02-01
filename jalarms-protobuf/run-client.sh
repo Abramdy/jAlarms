7f4b76e3cb396b5141dc14f24e388417bb20c7cb
Enrique Zamudio <alterbass@gmail.com>
2013-01-31 21:50:12 -0600

    update classpath of shell scripts

--- jalarms-protobuf/run-client.sh
+++ jalarms-protobuf/run-client.sh
@@ -1,5 +1,5 @@
 SPRING=`ls -1 lib/spring/*.jar|xargs|sed "s/ /:/g"`
 PROTOBUF=lib/protobuf.jar
-SLF4J=lib/slf4j-1.7.1/slf4j-api-1.7.1.jar:lib/slf4j-1.7.1/slf4j-simple-1.7.1.jar:lib/slf4j-1.7.1/jcl-over-slf4j-1.7.1.jar
+SLF4J=lib/slf4j-1.7.2/slf4j-api-1.7.2.jar:lib/slf4j-1.7.2/slf4j-simple-1.7.2.jar:lib/slf4j-1.7.2/jcl-over-slf4j-1.7.2.jar
 JALARMS=../jalarms-core/build/classes/main
 groovy -cp $SPRING:$PROTOBUF:$SLF4J:$JALARMS:build/classes/main src/test/groovy/TestClient.groovy
