74cd3674918e6dfd893db6ccada88de1f65dafb2
Enrique Zamudio <alterbass@gmail.com>
2015-05-25 17:12:46 -0500

    update versions

--- jalarms-protobuf/run-client.sh
+++ jalarms-protobuf/run-client.sh
@@ -1,5 +1,5 @@
 SPRING=`ls -1 lib/spring/*.jar|xargs|sed "s/ /:/g"`
 PROTOBUF=lib/protobuf.jar
-SLF4J=lib/slf4j-1.7.5/slf4j-api-1.7.5.jar:lib/slf4j-1.7.5/slf4j-simple-1.7.5.jar:lib/slf4j-1.7.5/jcl-over-slf4j-1.7.5.jar
+SLF4J=lib/slf4j-1.7.10/slf4j-api-1.7.10.jar:lib/slf4j-1.7.10/slf4j-simple-1.7.10.jar:lib/slf4j-1.7.10/jcl-over-slf4j-1.7.10.jar
 JALARMS=../jalarms-core/build/classes/main
 groovy -cp $SPRING:$PROTOBUF:$SLF4J:$JALARMS:build/classes/main src/test/groovy/TestClient.groovy
